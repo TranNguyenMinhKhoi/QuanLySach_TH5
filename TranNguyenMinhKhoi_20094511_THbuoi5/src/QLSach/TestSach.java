31d56fb19b13455ab9e0196ba28040d7eda46b95
iuh20094511 <98070818+TranNguyenMinhKhoi@users.noreply.github.com>
2024-09-18 14:57:40 +0700

    Update TestSach.java

--- TranNguyenMinhKhoi_20094511_THbuoi5/src/QLSach/TestSach.java
+++ TranNguyenMinhKhoi_20094511_THbuoi5/src/QLSach/TestSach.java
@@ -7,7 +7,7 @@ public class TestSach {
 		ListSach ls = new ListSach();
 		Sach s1 = new SachGiaoKhoa("SGK1", "Nhi Dong", LocalDate.of(2010, 04, 21), 20000, 10, false);
 		ls.themSach(s1);
-		Sach s2 = new SachThamKhao("SGK1", "Nhi Dong", LocalDate.of(2006, 04, 24), 50000, 30, "Canada");
+		Sach s2 = new SachThamKhao("STK1", "Phuong Nam", LocalDate.of(2006, 04, 24), 50000, 30, "Canada");
 		ls.themSach(s2);
 
 		System.out.println(s1.toString());
